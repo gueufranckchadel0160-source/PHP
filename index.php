1d611178b0a367974ffb5116889e54fbe9c0bbef
franck chadel <gueufranckchadel0160@gmail.com>
2026-03-03 22:14:56 +0000

    j'ai refaire de mon mieux

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <?php
     // EXERCICE 01
-    $integer="42";
-    $float="42.42";
-    $string="forty two";
-    $bool="true";
-    $null="NULL";
+$integer = 42;
+$float = 42.42;
+$string = "forty two";
+$bool = true;
+$null = NULL;
 
     // EXERCICE 02
+
+$my_array = [
+    "to",      // string
+    42,        // integer
+    "Glory",   // string
+    42.42,     // float
+    "Geckos"   // string
+];
+
+    // EXERCICE 03
+
+function my_concat($str1, $str2)
+{
+    echo $str1 . $str2;
+}
+
+
+
+    // EXERCICE 05
+    function swap(&$a, &$b)
+{
+    $temp = $a;
+    $a = $b;
+    $b = $temp;
+}
+
+    // EXERCICE 06
+    $func = function($str) {
+    return strtoupper($str);
+};
+
+
+
+    // EXERCICE 07
+
+
+$func = function($str) {
+    return strtoupper($str);
+};
 
 
 ?>
